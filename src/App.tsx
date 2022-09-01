a3fd6429127c0fe514f09d5d73bfa1fa8701bf60
ruben berhe <rubenberhe56@gmail.com>
2022-09-01 10:52:39 +0200

    add file components/Todo.tsx and modify app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@
 import React from "react";
 import Modal from "./components/Modal";
 import styled from "styled-components";
+import Todo from "./components/Todo";
 
 function App() {
 
 
   return (
     <>
-  
+     <Todo/>
       <Modal />
     </>
   );
