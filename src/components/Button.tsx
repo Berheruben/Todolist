c888bef889195d10620c51347d3367bb4309e80a
ruben berhe <rubenberhe56@gmail.com>
2022-09-04 20:50:05 +0200

    clean the button.tsx file code

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -42,18 +42,14 @@ export const Button: React.FC<ButtonProps> = ({
 }
 
 const StyledButton= styled.button`
-    border-color: transparent; 
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 16px 32px;
-    gap: 8px;
-    width: 263px;
-    height: 72px;
-    background: #716FB2;
-    // box-shadow: 4px 12px 24px rgba(0, 0, 0, 0.25);
-    border-radius: 32px;
-    flex: none;
-    order: 1;
-    flex-grow: 0;
-`
+border-color: transparent; 
+display: flex;
+justify-content: center;
+align-items: center;
+padding: 16px 32px;
+gap: 8px;
+width: 263px;
+height: 72px;
+background: #716FB2;
+border-radius: 32px;
+`;
